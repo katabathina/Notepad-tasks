83cc4c179784160079be7cb3c7c6ed385e1f3b95
katabathina <31954639+katabathina@users.noreply.github.com>
2018-03-16 22:56:31 +0530

    Imp notepads for all

---
+++ ec2create.bat
@@ -0,0 +1,10 @@
+aws cloudformation create-stack --template-body file://ec2_1.txt  --stack-name single1-instance --parameters ParameterKey=KeyName,ParameterValue=cfdemo ParameterKey=InstanceType,ParameterValue=t2.micro
+
+
+
+
+
+
+https://medium.com/boltops/a-simple-introduction-to-aws-cloudformation-part-1-1694a41ae59d
+
+cloudformation ec2 instance related links
